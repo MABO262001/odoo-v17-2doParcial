ca2dc8a1a4ea24de8a9068949feeae604b62febd
MABO262001 <ballivian.miguel@ficct.uagrm.edu.bo>
2024-06-16 17:53:42 -0400

    APIS INICIO

--- addons/ga/controllers/estudiante_controller.py
+++ addons/ga/controllers/estudiante_controller.py
@@ -1,8 +1,31 @@
 from odoo import http
 from odoo.http import request
+import json
 
 class EstudianteController(http.Controller):
 
-    @http.route('/open_cursos_view', type='http', auth='user', website=True)
-    def open_cursos_view(self):
-        return 1
+    @http.route('/ga/estudiantes', type='http', auth='public', methods=['GET'])
+    def get_estudiantes(self):
+        try:
+            estudiantes = request.env['gestion_academica.estudiante'].sudo().search([])
+            data = []
+            for estudiante in estudiantes:
+                data.append({
+                    'id': estudiante.id,
+                    'name': estudiante.name,
+                    'correo': estudiante.correo,
+                    'genero': estudiante.genero,
+                    'direccion': estudiante.direccion,
+                    'fecha_nacimiento': estudiante.fecha_nacimiento.strftime('%Y-%m-%d') if estudiante.fecha_nacimiento else '',
+                    'telefono': estudiante.telefono,
+                })
+            return request.make_response(
+                json.dumps(data),
+                headers=[('Content-Type', 'application/json')]
+            )
+        except Exception as e:
+            return request.make_response(
+                json.dumps({'error': str(e)}),
+                headers=[('Content-Type', 'application/json')],
+                status=500
+            )
